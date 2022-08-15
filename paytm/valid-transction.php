ab73a0fcde390b791ffe5aba13e606783c4af07c
jayaprasad356 <jayaprasad356@gmail.com>
2022-08-15 21:53:51 +0530

    makein

---
+++ paytm/valid-transction.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+    import checksum generation utility
+    You can get this utility from https://developer.paytm.com/docs/checksum/
+ */
+include_once '../includes/crud.php';
+include_once '../includes/custom-functions.php';
+include_once('../includes/variables.php');
+
+$db = new Database();
+$db->connect();
+$fn = new custom_functions();
+require_once("PaytmChecksum.php");
+
+/* initialize an array */
+$paytmParams = array();
+
+/* body parameters */
+$access_key = 90336;
+$paytm_merchantid = 'lItpNj25953497830204';
+$paytm_merchantkey = 'pD49rhdi&npTucum';
+$paytmParams["body"] = array(
+    /* Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+    "mid" => $paytm_merchantid,
+    /* Enter your order id which needs to be check status for */
+    "orderId" => $db->escapeString($fn->xss_clean($_POST['orderId'])),
+);
+
+/*
+    Generate checksum by parameters we have in body
+    Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+$checksum = PaytmChecksum::generateSignature(json_encode($paytmParams["body"]), $paytm_merchantkey);
+
+/* head parameters */
+$paytmParams["head"] = array(
+    /* put generated checksum value here */
+    "signature"    => $checksum
+);
+
+/* prepare JSON string for request */
+$post_data = json_encode($paytmParams);
+
+$url = "https://securegw.paytm.in/v3/order/status";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+$response = curl_exec($ch);
+print_r($response);
+return false;
